c875d411f3b4861ec9f3afd6dee3978c54783811
Theautis Persons <tptheautis@gmail.com>
2024-05-24 12:57:05 -0700

    set up ejs routes

--- src/NODE_LOGIN/server.js
+++ src/NODE_LOGIN/server.js
@@ -1,11 +1,25 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.render('index')
 });
+
+app.get("/users/register", (req, res) => {
+    res.render("register");
+})
+
+app.get("/users/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/users/dashboard", (req, res) => {
+    res.render("dashboard", { user: "Conor"});
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
