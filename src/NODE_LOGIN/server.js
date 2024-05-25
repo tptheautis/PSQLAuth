85a50ddd00b5307ab66bfbf91c33f9170d264ea5
Theautis Persons <tptheautis@gmail.com>
2024-05-25 12:19:51 -0700

    request body synced

--- src/NODE_LOGIN/server.js
+++ src/NODE_LOGIN/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 const app = express();
+import pool from './dbConfig.js';
 
 const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -11,16 +13,27 @@ app.get('/', (req, res) => {
 
 app.get("/users/register", (req, res) => {
     res.render("register");
-})
+});
 
 app.get("/users/login", (req, res) => {
     res.render("login");
-})
+});
 
 app.get("/users/dashboard", (req, res) => {
     res.render("dashboard", { user: "Conor"});
-})
+});
+
+app.post('/users/register', (req, res) => {
+    let { name, email, password, password2 } = req.body;
+
+    console.log(
+        name, 
+        email, 
+        password,
+        password2
+    )
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+});
